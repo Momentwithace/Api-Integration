70de32d4435d34d6302af1b7d023818c9406e3ef
momemtwithace <augustineezekiel763@gmail.com>
2023-03-06 22:52:10 +0100

    Updated App name

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Api Integration App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
